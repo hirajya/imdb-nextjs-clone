6d8fedc8db2f1fba5f1a3be30bd5a20d67a4e59c
hirajya <rodneyestrada2425@gmail.com>
2024-11-12 14:19:50 +0800

    About Page Done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to IMDb Clone — your personal movie collection companion! This
+        platform is designed for movie enthusiasts, collectors, and film critics
+        who want to organize their favorite films, explore new recommendations,
+        and stay updated on the latest releases, all in one place. Whether
+        you’re looking to log your watched movies, save a list of films to watch
+        later, or dive into detailed information about cast, crew, and reviews,
+        IMDb Clone has you covered. Start building your personalized movie
+        collection today and join a community that shares your love for cinema!
+      </p>
+    </div>
+  );
+}
